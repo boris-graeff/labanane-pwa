37892db4d8e8c7f09cab667ac34a078f64725d26
Boris <boris.graeff@gmail.com>
2018-06-25 13:03:32 +0200

    Setup api calls

---
+++ src/api/app.js
@@ -0,0 +1,13 @@
+import client from './client'
+
+const app = client('https://labanane.no-ip.info/services')
+
+export const createPlaylist = (params) => app.post('/playlists', params)
+
+export const getAllPlaylists = () => app.get('/playlists')
+
+export const getPlaylist = (id) => app.get(`/playlists/${id}`)
+
+export const updatePlaylist = (playlist) => app.put('/playlists', playlist)
+
+export const authenticate = (params) => app.post('/playlists/auth', params)
