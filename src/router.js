261804b01781293c937783a0809bfe714a6247fe
Boris <boris.graeff@gmail.com>
2018-06-25 12:59:41 +0200

    Setup router

--- src/router.js
+++ src/router.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Playlist from './views/Playlist.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/playlist/:playlistId/:trackId?',
+      name: 'playlist',
+      component: Playlist,
+      props: true
     }
   ]
 })
