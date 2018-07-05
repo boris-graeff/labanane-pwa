9cb73283d8c396230a7009ce0ea6d49ef47741b8
Boris <boris.graeff@gmail.com>
2018-07-05 12:41:03 +0200

    Create duration filter base on moment lib

---
+++ src/filters/duration.js
@@ -0,0 +1,14 @@
+import moment from 'moment'
+
+export default (duration) => {
+  if (!duration) return '-'
+
+  const momentDuration = moment.duration(duration)
+  let hours = momentDuration.hours()
+  let minutes = momentDuration.minutes()
+  let seconds = momentDuration.seconds()
+  hours = (hours) ? `${hours}:` : ''
+  minutes = (minutes < 10) ? `0${minutes}:` : `${minutes}:`
+  seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`
+  return hours + minutes + seconds
+}
