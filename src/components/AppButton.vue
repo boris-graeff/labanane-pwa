ee0ed5fa2bf107d629e8024bc013bd3c490ffae5
Boris Graeff <boris.graeff@gmail.com>
2018-10-15 12:25:54 +0200

    Create input and button components

---
+++ src/components/AppButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :type="type">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+  @import '~@/styles/constants';
+
+  button {
+    border: none;
+    padding: 6px 12px;
+    background: rgba($black, 0.1);
+    font-style: italic;
+    font-size: 18px;
+    font-weight: 700;
+    font-family: 'Roboto Condensed';
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+</style>
---
+++ src/components/AppInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input">
+    <label>
+      <span>{{ label }}</span>
+      <input v-bind="$attrs" v-on="$listeners" />
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['label']
+  }
+</script>
+
+
+<style scoped lang="scss">
+  @import '~@/styles/constants';
+
+  .input {
+    width: 100%;
+
+    label {
+      width: 100%;
+
+      span {
+        display: block;
+        font-size: 14px;
+        margin-bottom: 3px;
+        margin-top: 10px;
+      }
+    }
+
+    input {
+      background: rgba($black, 0.05);
+      border: 0;
+      border-bottom: 2px solid rgba($black, 0.1);
+      padding: $space-small;
+      height: 40px;
+      width: 100%;
+    }
+  }
+</style>
