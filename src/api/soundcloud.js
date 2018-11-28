07014720069d79e4886309dd0e0e80211af6b8ea
bof <boris.graeff@gmail.com>
2018-11-28 18:06:23 +0100

    Add track search on soundcloud

--- src/api/soundcloud.js
+++ src/api/soundcloud.js
@@ -1,5 +1,20 @@
 import constants from '@/constants.json'
+import axios from 'axios'
 
 const soundcloudApiKey = constants.SOUNDCLOUD_API_KEY
+const soundcloudBaseURL = 'https://api.soundcloud.com'
+const MAX_RESULTS = 20
 
-export const getTrackUrl = trackId => `https://api.soundcloud.com/tracks/${trackId}/stream?client_id=${soundcloudApiKey}`
+const soundcloud = axios.create({
+  baseURL: soundcloudBaseURL
+})
+
+export const getTrackUrl = trackId => `${soundcloudBaseURL}/tracks/${trackId}/stream?client_id=${soundcloudApiKey}`
+
+export const search = keywords => soundcloud.get('/tracks', {
+  params: {
+    q: keywords,
+    limit: MAX_RESULTS,
+    client_id: soundcloudApiKey
+  }
+})
--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -1,5 +1,6 @@
 import levenshtein from 'levenshtein'
 import { search as searchOnYoutube } from '@/api/youtube'
+import { search as searchOnSoundcloud } from '@/api/soundcloud'
 
 const SET_RESULTS = 'SET_RESULTS'
 
@@ -11,6 +12,18 @@ const youtubeSearch = async keywords => {
     provider: 'youtube',
     duration: 0,
     leven: levenshtein(track.snippet.title, keywords)
+  }))
+}
+
+const soundcloudSearch = async keywords => {
+  const results = await searchOnSoundcloud(keywords)
+  return results.data.map(track => ({
+    providerId: track.id,
+    name: track.title,
+    provider: 'soundcloud',
+    artwork: track.artwork_url,
+    duration: track.duration,
+    leven: levenshtein(track.title, keywords)
   }))
 }
 
@@ -30,7 +43,7 @@ export default {
     async searchTracks (store, keywords) {
       if (!keywords.length) store.commit(SET_RESULTS, [])
 
-      const results = await Promise.all([youtubeSearch(keywords)]);
+      const results = await Promise.all([youtubeSearch(keywords), soundcloudSearch(keywords)])
       store.commit(SET_RESULTS, results.flat().sort((a, b) => a.leven - b.leven))
     }
   }
